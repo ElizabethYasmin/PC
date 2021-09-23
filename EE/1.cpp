153d140ba603c112c8cf37f86600e0d2efd8ef5e
Beth40172 <ehuancap40172@gmail.com>
2021-09-22 23:54:23 -0500

    +

---
+++ EE/1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Anagrama(string c1,string c2){
+    int arreglo1[26]={0};
+    int arreglo2[26]={0};
+    if(c1.length()!=c2.length()){
+        return 0;
+    }
+    for(int i=0;c1[i]!='\0';i++){
+        arreglo1[c1[i]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(arreglo1[i]!=arreglo2[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+//Entrada: Sergio
+          //Mano
+//Salida : No anagramas
+int main(){
+    int n;
+    string c1,c2;
+    cout<<"caracter1";cin>>c1;
+    cout<<"caracter2";cin>>c2;
+    if(Anagrama(c1,c2)){
+        cout<<"anagramas";
+    }else{
+        cout<<"no anagramas";
+    }
+}
