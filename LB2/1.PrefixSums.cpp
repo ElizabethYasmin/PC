65e190e153570ddc7e9b428f72f6ec8e2f25d7f4
ElizabethYasmin <62725994+ElizabethYasmin@users.noreply.github.com>
2021-09-26 13:04:58 -0500

    +

--- LB2/1.PrefixSums.cpp
+++ LB2/1.PrefixSums.cpp
@@ -1,21 +1,25 @@
-//Prefix sums
-//#include<bits/stdc++.h>
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
-int a[100006];
-int n;
-int l,r;
-int q;
-int main(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>a[i];
-        a[i]+=a[i-1];
-    }
-    cin>>q;
-    for(int i=0;i<q;i++){
-        cin>>l>>r;
-        cout<<a[r]-a[l-1]<<endl;
-    }
-    return 0;
+ 
+
+void fillPrefixSum(int arr[], int n, int prefixSum[])
+{
+    prefixSum[0] = arr[0];
+ 
+   // Añadiendo el elemento presente  con elemento anterior
+    for (int i = 1; i < n; i++)
+        prefixSum[i] = prefixSum[i - 1] + arr[i];
+}
+ 
+
+int main()
+{
+    int arr[] = { 10, 4, 16, 20 };
+    const int n = sizeof(arr) / sizeof(arr[0]);
+    int prefixSum[n];
+ 
+    fillPrefixSum(arr, n, prefixSum);
+    for (int i = 0; i < n; i++)
+        cout << prefixSum[i] << " ";
 }
