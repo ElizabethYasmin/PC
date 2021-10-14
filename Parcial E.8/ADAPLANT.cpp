2e5766afeedf0cee0a0c4b7f74b4ad99c68aea0a
ElizabethYasmin <62725994+ElizabethYasmin@users.noreply.github.com>
2021-10-14 16:26:32 -0500

    ++

--- Parcial E.8/ADAPLANT.cpp
+++ Parcial E.8/ADAPLANT.cpp
@@ -1,126 +1,58 @@
-#include <iostream>
-#include <vector>
-#include <utility>
+#include <cstdio>
 #include <algorithm>
-#include <map>
-#include <iterator>
-#include <unordered_map>
-#include <stack>
-#include <queue>
+#define MAXN 100010
 using namespace std;
-
-typedef pair<int, int> ii;
-
-int A[200010];
-
-int main() {
-   int T, N, K;
-   int l, r;
-   int ans;
-   scanf("%d", &T);
-   while (T--) {
-      scanf("%d %d", &N, &K);
-      K++;
-      priority_queue<ii, vector<ii>, greater<ii>> Q1;
-      priority_queue<ii, vector<ii>> Q2;
-      for (int i = 0; i < N; i++)
-         scanf("%d", &A[i]);
-      l = 0;
-      r = min(N - 1, K);
-      for (int i = 0; i <= r; i++)
-         Q1.push(ii(A[i], i)), Q2.push(ii(A[i], i));
-      ans = 0;
-      while (l < N) {
-         while (Q1.top().second <= l - K - 1)
-            Q1.pop();
-         while (Q2.top().second <= l - K - 1)
-            Q2.pop();
-         ans = max(ans, max(Q2.top().first - A[l], A[l] - Q1.top().first));
-         l++;
-         if (r < N - 1) {
-            r++;
-            Q1.push(ii(A[r], r)), Q2.push(ii(A[r], r));
-         }
-      }
-      printf("%d\n", ans);
-   }
-   return 0;
+const int INF = 1e9 + 1;
+int arvore[4*MAXN],vetor[MAXN],n;
+void build(int pos,int left,int right){
+	if(left == right){
+		arvore[pos] = vetor[left];
+		return;
+	}
+	int mid = (left + right)/2;
+	build(2*pos,left,mid);
+	build(2*pos+1,mid+1,right);
+	arvore[pos] = min(arvore[2*pos],arvore[2*pos+1]);
+}
+int query(int pos,int left,int right,int i,int j){
+	if(left > right || left > j || right < i) return INF;
+	if(left >= i && right <= j){
+		return arvore[pos];
+	}
+	int mid = (left + right)/2;
+	return min(query(2*pos,left,mid,i,j),query(2*pos+1,mid+1,right,i,j));
 }
 
 
-/*   #include<stdio.h>
-    #include<bits/stdc++.h>
-    using namespace std;
-    typedef struct Node{
-    	long long maxx;
-    	long long minn;
-    }node;
-    node seg[400005];
-    long long a[100005];
-    node fn(int l, int r, int ind){
-    	node temp;
-    	if(l==r){
-    		temp.maxx=a[l];
-    		temp.minn=a[l];
-    		return temp;
-    	}
-    	seg[2*ind]=fn(l, (l+r)/2, 2*ind);
-    	seg[2*ind+1]=fn(((l+r)/2)+1, r, 2*ind+1);
-    	temp.maxx=max(seg[2*ind].maxx, seg[2*ind+1].maxx);
-    	temp.minn=min(seg[2*ind].minn, seg[2*ind+1].minn);
-    	return temp;
-    }
-    long long Searchx(int l, int r, int s, int e, int ind){
-    	if(l<=s&&r>=e){
-    		return seg[ind].maxx;
-    	}
-    	if(l>(s+e)/2){
-    		return Searchx(l, r, (s+e)/2+1, e, 2*ind+1);
-    	}
-    	if(r<=(s+e)/2){
-    		return Searchx(l, r, s, (s+e)/2, 2*ind);
-    	}
-    	else{
-    		return max(Searchx(l, (s+e)/2, s, (s+e)/2, 2*ind), Searchx((s+e)/2+1, r, (s+e)/2+1, e, 2*ind+1));
-    	}
-    }
-    long long Searchn(int l, int r, int s, int e, int ind){
-    	if(l<=s&&r>=e){
-    		return seg[ind].minn;
-    	}
-    	if(l>(s+e)/2){
-    		return Searchn(l, r, (s+e)/2+1, e, 2*ind+1);
-    	}
-    	if(r<=(s+e)/2){
-    		return Searchn(l, r, s, (s+e)/2, 2*ind);
-    	}
-    	else{
-    		return min(Searchn(l, (s+e)/2, s, (s+e)/2, 2*ind), Searchn((s+e)/2+1, r, (s+e)/2+1, e, 2*ind+1));
-    	}
-    }
-    int main(){
-    	int T;
-    	scanf("%d", &T);
-    	while(T--){
-    		int n, k;
-    		scanf("%d %d", &n, &k);
-    		for(int i=0;i<n;i++){
-    			scanf("%lld", &a[i]);
-    		}
-    		seg[1]=fn(0, n-1, 1);
-    		long long currmax=-1;
-    		if(k>n-2){
-    			printf("%lld\n", Searchx(0, n-1, 0, n-1, 1)-Searchn(0, n-1, 0, n-1, 1));
-    			continue;
-    		}
-    		for(int i=k+1;i<n;i++){
-    			if(Searchx(i-k-1, i, 0, n-1, 1)-Searchn(i-k-1, i, 0, n-1, 1)>currmax){
-    				currmax=Searchx(i-k-1, i, 0, n-1, 1)-Searchn(i-k-1, i, 0, n-1, 1);
-    			}
-    		}
-    		printf("%lld\n", currmax);
-    	}
-    	return 0;
-    }*/
+//Entrada:
+//3
+//5 0
+//1 2 3 5 6
+//4 6
+//1 10 2 9
+//10 1
+//1 7 8 9 19 11 21 8 11 0 
+//Salida:
+//2
+//9
+//13
 
-    
+int main(){
+	int TC;
+	scanf("%d",&TC);
+	while(TC--){
+		int k;
+		scanf("%d %d",&n,&k);
+		for(int i=1;i<=n;i++){
+			scanf("%d",&vetor[i]);
+		}
+		int resp = 0;
+		build(1,1,n);
+		for(int i=1;i<=n;i++){
+			int menor = min(query(1,1,n,max(1,i - k - 1),max(i-1,1)), query(1,1,n,min(i+1,n),min(i+k+1,n)));
+			resp = max(vetor[i] - menor,resp);
+		}
+		printf("%d\n",resp);
+	}
+	return 0;
+}
