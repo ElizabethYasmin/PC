65517d21403a5e8b7c50f4344d9a08827a65f24a
ElizabethYasmin <elizabethhuanca40172@gmail.com>
2021-10-21 20:36:20 -0500

    actualizando Minimum add to make

--- PC_Aula_10/Evaluate Reverse Polish Notation/EvaluateReversePolishNotation.cpp
+++ PC_Aula_10/Evaluate Reverse Polish Notation/EvaluateReversePolishNotation.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-int evalRPN(vector<string>& token) {
+int evalRPN(vector<string>& tok) {
         stack<int> numeros;
-        for(const string& s: token){
+        for(const string& s: tok){
             if(s == "+" || s == "-" || s == "*" || s == "/"){
                 int a = numeros.top();
                 numeros.pop();
@@ -28,9 +28,12 @@ int evalRPN(vector<string>& token) {
         }
         return numeros.top();
 }
+//Input: tokens = ["2","1","+","3","*"]
+//Output: 9
+//Explanation: ((2 + 1) * 3) = 9
 
 int main() {
     vector<string> tokens = {"2","1","+","3","*"};
-    cout<<evalRPN(tokens);
+    evalRPN(tokens);
     return 0;
 }
--- PC_Aula_10/Minimum Add to Make Parentheses Valid/MinimumAddtoMakeParenthesesValid.cpp
+++ PC_Aula_10/Minimum Add to Make Parentheses Valid/MinimumAddtoMakeParenthesesValid.cpp
@@ -1,26 +1,32 @@
 
 #include <stdio.h>
-class Solution {
-public:
-    int minAddToMakeValid(string S) {
-        int ans = 0;
-        stack<int> s;
-        for(int i=0;i<S.size();i++){
-            if(S[i] == '(')
-                s.push(i);
-            else if(s.empty())
-                ans++;
-            else
-                s.pop();
-        }
-        ans += s.size();
-        return ans;
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+int minAddToMakeValid(string S) {
+    int ans = 0;
+    stack<int> s;
+    for(int i=0;i<S.size();i++){
+        if(S[i] == '(')
+            s.push(i);
+        else if(s.empty())
+            ans++;
+        else
+            s.pop();
     }
-};
+    ans += s.size();
+    return ans;
+}
+//Input: s = "())"
+//Output: 1
 
 int main()
 {
-    printf("Hello World");
-
+    string s ={"())"};
+    cout<<minAddToMakeValid(s);
     return 0;
 }
