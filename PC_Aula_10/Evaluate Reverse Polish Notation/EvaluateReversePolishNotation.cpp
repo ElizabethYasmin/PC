c2f7855030e55f560e20b836517d1137565da1c9
ElizabethYasmin <elizabethhuanca40172@gmail.com>
2021-10-21 20:17:39 -0500

    actualizando Evaluate Reverse Polish Notation

--- PC_Aula_10/Evaluate Reverse Polish Notation/EvaluateReversePolishNotation.cpp
+++ PC_Aula_10/Evaluate Reverse Polish Notation/EvaluateReversePolishNotation.cpp
@@ -4,36 +4,33 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int evalRPN(vector<string>& tokens) {
-
-        stack<int> nums;
-        for(const string& s: tokens){
+int evalRPN(vector<string>& token) {
+        stack<int> numeros;
+        for(const string& s: token){
             if(s == "+" || s == "-" || s == "*" || s == "/"){
-                int a = nums.top();
-                nums.pop();
-                int b = nums.top();
-                nums.pop();
-
-                if(s == "+")
-                    nums.push(b + a);
-                else if(s == "-")
-                    nums.push(b - a);
-                else if(s == "*")
-                    nums.push(b * a);
-                else if(s == "/")
-                    nums.push(b / a);
+                int a = numeros.top();
+                numeros.pop();
+                int b = numeros.top();
+                numeros.pop();
+                if(s == "+"){
+                    numeros.push(b + a);
+                }else if(s == "-"){
+                    numeros.push(b - a);
+                }else if(s == "*"){
+                    numeros.push(b * a);
+                }else if(s == "/"){
+                    numeros.push(b / a);
+                }
+             }else{
+                numeros.push(atoi(s.c_str()));
             }
-            else
-                nums.push(atoi(s.c_str()));
+                
         }
-        return nums.top();
-    }
-};
-
+        return numeros.top();
+}
 
 int main() {
-
+    vector<string> tokens = {"2","1","+","3","*"};
+    cout<<evalRPN(tokens);
     return 0;
 }
