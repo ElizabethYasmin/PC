5ab4d41cd59e2f3dc25ec01db81c229ecf851578
ElizabethYasmin <elizabethhuanca40172@gmail.com>
2021-11-27 22:13:19 -0500

    broken

--- Examen2PC/P1_Stack Construction/P1_Stack_Construction.cpp
+++ Examen2PC/P1_Stack Construction/P1_Stack_Construction.cpp
@@ -1,7 +1,44 @@
 //https://open.kattis.com/problems/stack//
 
+#include <iostream>
+#include <string>
+using namespace std;
+int memo[300+1][300+1];
 
+int function(const string &s, int start, int end){
+  int &ans = memo[start][end];
+  if (ans != -1)
+    return ans;
+  if (start == end)
+    return ans = 2;
+  ans = 2*(end-start+1);
+  ans = min(ans, 2 + function(s, start, end-1));
+  for (int i = start; i < end; i++) {
+    if (s[i] == s[end])
+      ans = min(ans, function(s, start, i) + function(s, i+1, end-1));
+  }
+  return ans;
+}
 
+void solution(){
+  string s;
+  getline(cin, s);
+  int n = s.length();
+  for (int i = 0; i <= n; i++) {
+    fill(memo[i], memo[i]+n+1, -1);
+  }
+  cout << n + function(s, 0, n-1) << endl;
+}
+
+int main(){
+  int T;
+  cin >> T;
+  string line;
+  getline(cin, line);
+  while (T--)
+    solution();
+  return 0;
+}
 
 
 
---
+++ Examen2PC/P2_Broken Keyboard/P2_Broken_Keyboard.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+
+using namespace std;
+typedef map<char,int> tMCI;
+
+int main(){
+	int m;
+	while (cin >> m && m){
+		cin.ignore();
+		string line;
+		getline(cin, line);
+		int si = 0;
+		int ei = 0;
+		int diff = 0;
+		int res = 0;
+		tMCI mp;
+		while (1){
+			if (si >= (int) line.length()) break;
+			char c = line[si];
+			if (diff < m){
+				if (mp.find(c) == mp.end()){
+					++diff;
+				}
+				++mp[c];
+			}
+			else{
+				if (mp.find(c) != mp.end()){	
+					++mp[c];
+				}else{
+					while (diff == m){
+						char ec = line[ei];
+						--mp[ec];
+						if (mp[ec] == 0){
+							mp.erase(mp.find(ec));
+							--diff;
+						}
+						++ei;
+					}
+					++mp[c];
+					++diff;
+				}
+			}
+			++si;
+			res = max(si-ei, res);
+		}
+		cout << res << endl;
+	}
+}
