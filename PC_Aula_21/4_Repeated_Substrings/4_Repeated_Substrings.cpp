84dcec88e6289b74bfadf75cb38d11ead932b513
ElizabethYasmin <elizabethhuanca40172@gmail.com>
2021-12-25 22:03:12 -0500

    Pruebas Aula 21

--- PC_Aula_21/4_Repeated_Substrings/4_Repeated_Substrings.cpp
+++ PC_Aula_21/4_Repeated_Substrings/4_Repeated_Substrings.cpp
@@ -1,63 +1,111 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
-//#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+struct SuffixArray {
+	string s;
+	int n;
+	vector < vector <int> > P;
+	SuffixArray(string &_s) : s(_s), n(_s.length()) {
+    construct();
+  }
 
-const long long int limi=1e5+5;
-int n=0,t=0,ans=0;
-string arr;
-int sa[limi],lcp[limi];
-int tmp[limi],pos[limi];
+void construct() {
+		P.push_back(vector <int> (n, 0));
+		compress();
+		vector <int> occ(n + 1, 0), s1(n, 0), s2(n, 0);
+		for ( int k = 1, cnt = 1; cnt / 2 < n; ++k, cnt *= 2 ) {
+			P.push_back(vector <int> (n, 0));
+			fill(occ.begin(), occ.end(), 0);
+			for ( int i = 0; i < n; ++i )
+				occ[i+cnt<n ? P[k-1][i+cnt]+1 : 0]++;
 
-bool COMPAR(int i, int j){
-    if(pos[i]!=pos[j]) return (pos[i]<pos[j]);
-    i+=t;j+=t;
-    if(i<n && j<n) return (pos[i]<pos[j]);
-    return (i>j);
+			partial_sum(occ.begin(), occ.end(), occ.begin());
+
+			for ( int i = n - 1; i >= 0; --i )
+				s1[--occ[i+cnt<n ? P[k-1][i+cnt]+1 : 0]] = i;
+
+			fill(occ.begin(), occ.end(), 0);
+
+			for ( int i = 0; i < n; ++i )
+				occ[P[k-1][s1[i]]]++;
+
+			partial_sum(occ.begin(), occ.end(), occ.begin());
+
+			for ( int i = n - 1; i >= 0; --i )
+				s2[--occ[P[k-1][s1[i]]]] = s1[i];
+
+			for ( int i = 1; i < n; ++i ) {
+				P[k][s2[i]] = same(s2[i], s2[i - 1], k, cnt)
+					? P[k][s2[i - 1]] : i;
+			}
+		}
+	}
+
+bool same(int i, int j, int k, int l) {
+	return P[k - 1][i] == P[k - 1][j]
+		&& (i + l < n ? P[k - 1][i + l] : -1)
+		== (j + l < n ? P[k - 1][j + l] : -1);
 }
-void SUFFIXARRAY(){
-    for(int i=0; i<n;i++){
-        sa[i]=i;
-        pos[i]=arr[i]; }
-    for(t=1;t<2*n+1;t*=2){
-        sort(sa,sa+n,COMPAR);
-        for(int i=0; i<n-1;i++){
-            tmp[i+1]=tmp[i];
-            if(COMPAR(sa[i],sa[i+1])) tmp[i+1]++; }
-        for(int i=0; i<n;i++){
-            pos[sa[i]]=tmp[i];}
-        if(tmp[n-1]==n-1) break; }
+
+void compress() {
+	vector <int> cnt(256, 0);
+	for ( int i = 0; i < n; ++i )
+    cnt[s[i]]++;
+
+	for ( int i = 0, mp = 0; i < 256; ++i )
+		  if (cnt[i] > 0) cnt[i] = mp++;
+	for ( int i = 0; i < n; ++i )
+       P[0][i] = cnt[s[i]];
 }
 
-void LCP_FIND(){
-    int p=0;
-    for(int i=0; i<n;i++){
-        if(pos[i]!=n-1){
-            int j=sa[pos[i]+1];
-            while(arr[i+p]==arr[j+p])p++;
-            lcp[pos[i]]=p;
-            if(p>0) p--;
-        }
-    }
+vector <int> &get_array() {
+      return P.back();
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int x=0;
-    cin>>x;
-    
-    for(int i=0;i<x;i++){
-        cin>>arr;
-        n=arr.length();
-        ans=lcp[0];
-        for(int j=1; j<n;j++){
-            if(lcp[j]>lcp[j-1]) {
-                ans+=(lcp[j]-lcp[j-1]); }
-        }
-        cout<<ans<<"\n";
-    }
-    return 0;
+int lcp(int x, int y) {
+	int ret = 0;
+	if (x == y)
+     return n - x;
+	for ( int k = P.size() - 1; k >= 0 && x < n && y < n; --k )
+     if (P[k][x] == P[k][y]) {
+         x += 1 << k;
+         y += 1 << k;
+         ret += 1 << k;
+		}
+	return ret;
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+	while (T--) {
+		string s;
+		cin >> s;
+		int n = s.length();
+		SuffixArray sa(s);
+
+		vector <int> &arr = sa.get_array();
+
+		vector <int> inv(n, -1);
+		for ( int i = 0; i < n; ++i ) {
+			inv[arr[i]] = i;
+		}
+
+		vector <int> lcp(n, 0);
+		for ( int i = 1; i < n; ++i ) {
+			lcp[i] = sa.lcp(inv[i - 1], inv[i]);
+		}
+
+		long long ans = 0;
+		for ( int i = 1; i < n; ++i ) {
+        if ( lcp[i] > lcp[i - 1] )
+          ans += lcp[i] - lcp[i - 1];
+		}
+
+		cout << ans << endl;
+	}
+	return 0;
 }
